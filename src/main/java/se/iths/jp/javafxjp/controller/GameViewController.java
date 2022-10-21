024db210849542bd4903a292e619151809e979a9
dzJP <113700179+dzJP@users.noreply.github.com>
2022-10-21 15:36:02 +0200

    Add snake growing

--- src/main/java/se/iths/jp/javafxjp/controller/GameViewController.java
+++ src/main/java/se/iths/jp/javafxjp/controller/GameViewController.java
@@ -2,6 +2,8 @@ package se.iths.jp.javafxjp.controller;
 
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import se.iths.jp.javafxjp.model.GameAnimation;
@@ -17,28 +19,43 @@ public class GameViewController {
 
     public void initialize() {
         context = canvas.getGraphicsContext2D();
+        canvas.setFocusTraversable(true);
         gameAnimation.start();
     }
 
+    //Todo: Keyboard input
 
     GameAnimation gameAnimation = new GameAnimation() {
         float time;
+
         @Override
         public void tick(float secondsSinceLastFrame) {
             time += secondsSinceLastFrame;
             if (time < 0.2)
                 return;
 
+            snake.update();
             render();
+            time = 0.0f;
         }
     };
 
 
     public void render() {
         context.setFill(Color.web("#eddeaf"));
-        context.setFill(Color.YELLOW);
-        context.fillRect(0,0,400,400);
-        context.setFill(Color.BLUE);
-        context.fillRect((snake.getPosition().x() * 10),(snake.getPosition().y() * 10), 5,5);
+        context.fillRect(0, 0, 400, 400);
+        context.setFill(Color.web("#004B87"));
+        for (var segment : snake.getPositionList())
+            context.fillRect((segment.x() * 10), (segment.y() * 10), 10, 10);
+    }
+
+    public void keyPressed(KeyEvent keyEvent) {
+        System.out.println("Key pressed");
+        switch (keyEvent.getCode()) {
+            case UP -> snake.setUp();
+            case DOWN -> snake.setDown();
+            case LEFT -> snake.setLeft();
+            case RIGHT -> snake.setRight();
+        }
     }
 }
